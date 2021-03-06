6565239b47a3ab34f6f15ebd5fea95b8db8495f9
Billa1021 <80149506+Billa1021@users.noreply.github.com>
2021-03-06 20:39:31 +0530

    Update churn.py

--- churn.py
+++ churn.py
@@ -47,7 +47,6 @@ from sklearn.model_selection import train_test_split
 
 X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=0)
 
-from sklearn.linear_model import LogisticRegression
 
 reg = LogisticRegression(class_weight= {0:1,1:3})
 
